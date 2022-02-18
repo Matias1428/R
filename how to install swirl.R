4cc3663d82c759eadf297b6df4969f90ecfaa4cb
Matias1428 <82426187+Matias1428@users.noreply.github.com>
2022-02-18 15:17:56 -0600

    First

---
+++ how to install swirl.R
@@ -0,0 +1,16 @@
+
+#1. Install swirl
+#Since swirl is an R package, you can easily install it by entering a single command from the R console:
+
+install.packages("swirl")
+
+
+#2. Load swirl
+#Every time you want to use swirl, you need to first load the package. From the R console:
+
+packageVersion("swirl")
+
+library(swirl)
+
+install_from_swirl("R Programming")
+swirl()
